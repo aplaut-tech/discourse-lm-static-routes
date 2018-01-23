ff1e13a4a023cab307ad7775afba9dff031a16b1
Alexander Chaychuk <mr.sashich@gmail.com>
2018-01-23 15:37:29 +0300

    Fix routes again

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 after_initialize do
-  Rails.application.routes.draw do
+  Discourse::Application.routes.draw do
     get "rules" => "static#show", id: "tos", as: 'rules', constraints: { format: /(json|html)/ }
   end
 end
